0b05b16be64bebc09d3ff8dcfb8c34707fbf1d84
Rodrigo Vizcarra <Rodrigo.vizcarra@gmail.com>
2023-04-19 23:51:04 -0700

    updated functionality

--- scripts/lab1-create-users.ps1
+++ scripts/lab1-create-users.ps1
@@ -1,39 +1,62 @@
-# Import the Active Directory module
 Import-Module ActiveDirectory
 
-# Define the paths to account data files
+# Function AD groups based text file
+function New-ADGroupsFromFile {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$path
+    )
+
+    $groups = Get-Content $path
+    foreach ($group in $groups) {
+        if (!(Get-ADGroup -Filter { Name -eq $group })) {
+            New-ADGroup -Name $group -GroupScope Global
+        }
+    }
+}
+
+# Variables from data files
 $firstNameFile = ".\resources\firstnames.csv"
 $lastNameFile = ".\resources\surnames.csv"
 $passwordFile = ".\resources\passwords.txt"
 $domainGroupsFile = ".\resources\domain-groups.txt"
 
-# Prompt the user for the number of users to create
+# User Prompts
 $UserCount = Read-Host "How many user accounts do you want to create? "
+$domainName = Read-Host "What is the lab domain name? "
 
-# Loop based on user input
+# Create AD groups
+New-ADGroupsFromFile -Path $domainGroupsFile
+
+# Create user for loop engine
 for ($i = 1; $i -le $UserCount; $i++) {
-    $firstName = (Get-Content $firstNameFile | Get-Random).Trim()
-    $lastName = (Get-Content $lastNameFile | Get-Random).Trim()
-    $password = (Get-Content $passwordFile | Get-Random).Trim()
+    $firstName = (Get-Content $firstNameFile | Get-Random).Trim("""")
+    $lastName = (Get-Content $lastNameFile | Get-Random).Trim("""")
+    $_password = (Get-Content $passwordFile | Get-Random).Trim()
     $domainGroup = (Get-Content $domainGroupsFile | Get-Random).Trim()
-    Write-Host "Creating user: $($firstName)" -BackgroundColor Black -ForegroundColor Cyan
+    $_username = ($firstName.Substring(0,1) + $lastName).ToLower()
+    $password = ConvertTo-SecureString $_password -AsPlainText -Force
+
+    Write-Host "Creating user: $($firstName) $($lastName) $($_password)" -BackgroundColor Black -ForegroundColor Cyan
 
     # Define the user properties
     $userProperties = @{
-        Name = $firstName + "." + $lastName
+        Name = ($firstName.Substring(0,1) + $lastName).ToLower()
         GivenName = $firstName
         Surname = $lastName
         DisplayName = $firstName + " " + $lastName
-        UserPrincipalName = $firstName + "." + $lastName + "@arconyhcal.dev"
+        UserPrincipalName = $firstName + "." + $lastName + "@" + $domainName
         AccountPassword = ConvertTo-SecureString $password -AsPlainText -Force
         Enabled = $true
         ChangePasswordAtLogon = $false
+        # Set the sAMAccountName to the first initial of the first name and the last name
         SamAccountName = ($firstName.Substring(0,1) + $lastName).ToLower()
+   
     }
 
-    # Create the user account in Active Directory
+    # Create account & add user to random group
     New-ADUser @userProperties
-
-    # Add the user to the random domain group
     Add-ADGroupMember -Identity $domainGroup -Members $userProperties.Name
-}
+        
+    }
