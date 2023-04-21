f442b5a891f1cf464974b97bd275712bd891bb40
Rodrigo Vizcarra <Rodrigo.vizcarra@gmail.com>
2023-04-20 20:41:19 -0700

    Updates to create forest script

--- scripts/lab1-create-forest.ps1
+++ scripts/lab1-create-forest.ps1
@@ -1,29 +1,19 @@
 # This utility prompts the user and creates a domain based on input
 # Prompt
 $domainName = Read-Host -Prompt 'Enter the domain name '
-
-# Setup Variables
-$databasePath = 'C:\Windows\NTDS'
-$domainMode = 'WinThreshold'
 $domainNetbiosName = $domainName.Split('.')[0].ToUpper()
-$forestMode = 'WinThreshold'
-$installDns = $true
-$logPath = 'C:\Windows\NTDS'
-$noRebootOnCompletion = $false
-$sysvolPath = 'C:\Windows\SYSVOL'
-$force = $true
 
 # Install ADDS Forest
 Import-Module ADDSDeployment
 Install-ADDSForest `
-    -CreateDnsDelegation:$false `
-    -DatabasePath $databasePath `
-    -DomainMode $domainMode `
-    -DomainName $domainName `
-    -DomainNetbiosName $domainNetbiosName `
-    -ForestMode $forestMode `
-    -InstallDns $installDns `
-    -LogPath $logPath `
-    -NoRebootOnCompletion $noRebootOnCompletion `
-    -SysvolPath $sysvolPath `
-    -Force $force
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName $domainName `
+-DomainNetbiosName $domainNetbiosName `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
